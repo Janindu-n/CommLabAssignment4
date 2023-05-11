ce916e55d6eb544520b286bdb466725f484c37ab
Janindu-n <jin3303@nyu.edu>
2023-05-11 08:06:20 +0400

    final edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
-  })
+if (document.getElementById("my-work-link")) {
+  document.getElementById("my-work-link").addEventListener("click", () => {
+    document
+      .getElementById("my-work-section")
+      .scrollIntoView({ behavior: "smooth" });
+  });
 }
+
+var imageContainer = document.getElementById(
+  "portfolio-header-image-container"
+);
+var image = document.querySelector(".portfolio-header-image");
+
+imageContainer.addEventListener("click", function () {
+  image.classList.toggle("flipped");
+});
+
+window.addEventListener("scroll", function () {
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var percentage = (scrollPosition / height) * 100;
+  var hue = Math.floor(percentage * 2.55); // Convert the percentage to hue value (0-255)
+
+  document.body.style.backgroundColor = "hsl(" + hue + ", 70%, 80%)";
+});
